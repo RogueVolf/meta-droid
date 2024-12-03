cac3f46da53866568e334d19ea034fe715c04483
RogueVolf <abrar123.aaaa@gmail.com>
2024-12-03 16:07:17 +0530

    Adding Perfume Mockups

--- constants/index.js
+++ constants/index.js
@@ -1,29 +1,19 @@
 export const exploreWorlds = [
   {
     id: 'world-1',
-    imgUrl: '/planet-01.png',
-    title: 'The Hogwarts',
+    imgUrl: '/perfume_1.png',
+    title: 'The Chemistry',
   },
   {
     id: 'world-2',
-    imgUrl: '/planet-02.png',
-    title: 'The Upside Down',
+    imgUrl: '/perfume_2.png',
+    title: 'The Misty',
   },
   {
     id: 'world-3',
-    imgUrl: '/planet-03.png',
-    title: 'Hueco Mundo',
-  },
-  {
-    id: 'world-4',
-    imgUrl: '/planet-04.png',
-    title: 'Paradise Island',
-  },
-  {
-    id: 'world-5',
-    imgUrl: '/planet-05.png',
-    title: 'Hawkins Labs',
-  },
+    imgUrl: '/perfume_3.png',
+    title: 'The Macho',
+  }
 ];
 
 export const startingFeatures = [
